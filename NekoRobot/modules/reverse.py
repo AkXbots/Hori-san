b35ecd9b28672c1c91737dd72ccfcc4fb5551b99
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-05-08 20:22:54 +0530

    Update reverse.py

--- NekoRobot/modules/reverse.py
+++ NekoRobot/modules/reverse.py
@@ -1,69 +1,90 @@
-# Author: Sasta Dev || https://t.me/SastaDev
-# Author's github: https://github.com/SastaDev
-# Created on: Saturday, 29 April, 2023.
-# Written in python using pyrogram.
-# This python script only supports pyrogram versions lower than v2.0.
-# © Sasta Dev ~ 2023.
+import requests
+from pyrogram import filters, client  # Pyrogram v2.0 and up
+from pyrogram.enums import ParseMode
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-import uuid
-from pyrogram import Client, filters
-from pyrogram.types import (InlineKeyboardMarkup, InlineKeyboardButton, Message)
-import httpx
+from NekoRobot import pgram as pbot, TOKEN
 
-from NekoRobot import pgram
+API_URL = "https://google-reverse-image-api.vercel.app/reverse"
 
-API_URL = 'https://sasta.tk/google_reverse'
 
-class STRINGS:
-    REPLY_TO_MEDIA = 'Reply to a message having photo, sticker or document.'
-    THESE_MEDIA_TYPES_ONLY = 'Only <b>photo</b>, <b>sticker</b> and <b>document</b> media types are allowed.'
-    GIF_NOT_SUPPORTED = 'GIF reverse is currently not available.'
-    DOWNLOADING_MEDIA = '<b>• Downloading media...</b>'
-    UPLOADING_MEDIA = '<b>• Uploading media...</b>'
-    API_ERROR = '<b>An API Error occured while requesting:</b>:\n{}'
-    SUPPORT_CHAT = '<b>Support Chat:</b> @HelpSupportChat'
-    REVERSE_RESULT = '''
-<b>Search Keyword:</b> <code>{}</code>
-<b>Results link:</b> <a href='{}'>Link</a>.
-
-<b>Credits:</b> @SastaDev
-    '''
-
-COMMANDS = ['reverse', 'grs', 'pp']
-
-@pgram.on_message(filters.command(COMMANDS))
-async def on_reverse(client: Client, message: Message) -> Message:
-    if not message.reply_to_message and not message.reply_to_message.media:
-        await message.reply(STRINGS.REPLY_TO_MEDIA)
-        return
-    media_type = message.reply_to_message.media
-    if media_type not in ('photo', 'sticker', 'document'):
-        if media_type == 'animation':
-            await message.reply(STRINGS.GIF_NOT_SUPPORTED)
+async def get_file_id_from_message(message: Message):
+    file_id = None
+    message = message.reply_to_message
+    if not message:
+        return None
+    if message.document:
+        if int(message.document.file_size) > 3145728:
             return
-        await message.reply(STRINGS.THESE_MEDIA_TYPES_ONLY)
-        return
-    status_msg = await message.reply(STRINGS.DOWNLOADING_MEDIA)
-    file_path = f'downloads/{uuid.uuid4()}'
-    await message.reply_to_message.download(file_path)
-    await status_msg.edit(STRINGS.UPLOADING_MEDIA)
-    async with httpx.AsyncClient(timeout=30) as async_client:
-        with open(file_path, 'rb') as file:
-            files = {'file': file}
-            response = await async_client.post(API_URL, files=files)
-        try:
-            response_json = response.json()
-        except:
-            await message.reply(STRINGS.API_ERROR.format(response.text) + '\n\n' +  STRINGS.SUPPORT_CHAT)
+        mime_type = message.document.mime_type
+        if mime_type not in ("image/png", "image/jpeg"):
             return
-        if response.status_code != 200:
-            await message.reply(STRINGS.API_ERROR.format(response_json['error']) + '\n\n' + STRINGS.SUPPORT_CHAT)
+        file_id = message.document.file_id
+
+    if message.sticker:
+        if message.sticker.is_animated:
+            if not message.sticker.thumbs:
+                return
+            file_id = message.sticker.thumbs[0].file_id
+        else:
+            file_id = message.sticker.file_id
+
+    if message.photo:
+        file_id = message.photo.file_id
+
+    if message.animation:
+        if not message.animation.thumbs:
             return
-        search_keyword = response_json['keyword']
-        url = response_json['url']
-    text = STRINGS.REVERSE_RESULT.format(search_keyword, url)
-    reply_markup = [
-        [InlineKeyboardButton('Open Link', url=url)]
-        ]
-    await message.reply(text, reply_markup=InlineKeyboardMarkup(reply_markup))
-    await status_msg.delete()
+        file_id = message.animation.thumbs[0].file_id
+
+    if message.video:
+        if not message.video.thumbs:
+            return
+        file_id = message.video.thumbs[0].file_id
+    return file_id
+
+
+@pbot.on_message(filters.command(["pp", "grs", "reverse"]))
+async def reverse(app: client, msg: Message):
+    text = await msg.reply("```Parsing Media...```", parse_mode=ParseMode.MARKDOWN)
+    file_id = await get_file_id_from_message(msg)
+    if not file_id:
+        return await text.edit(
+            "Reply to a Photo or sticker", parse_mode=ParseMode.MARKDOWN
+        )
+    await text.edit("```Searching...```", parse_mode=ParseMode.MARKDOWN)
+
+    r = requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}"
+    ).json()
+    file_path = r["result"]["file_path"]
+
+    data = {
+        "imageUrl": f"https://images.google.com/searchbyimage?safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    }
+
+    response = requests.post(API_URL, json=data)
+
+    if response.status_code == 200:
+        result = response["data"]
+        return await text.edit(
+            f'Sauce: ```{result["output"]}```',
+            parse_mode=ParseMode.MARKDOWN,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Link", url=result["similar"])]]
+            ),
+        )
+    elif response.status_code == 401:
+        return await text.edit(
+            "```Couldn't find anything```", parse_mode=ParseMode.MARKDOWN
+        )
+    elif response.status_code == 402:
+        return await text.edit(
+            "```Failed to reverse image```", parse_mode=ParseMode.MARKDOWN
+        )
+    elif response.status_code <= 500:
+        return await text.edit("```Error in API```", parse_mode=ParseMode.MARKDOWN)
+    else:
+        return await text.edit(
+            "```Unknown Error Occurred```", parse_mode=ParseMode.MARKDOWN
+        )
